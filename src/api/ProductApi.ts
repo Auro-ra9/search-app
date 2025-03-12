11aecd6e94ac2b83b3b2d2ff3f45a3839651e4a0
Auro-ra9 <ashnaahammed9@gmail.com>
2025-03-12 19:30:50 +0530

    fix: resolve 'id' property error by defining proper type for product data

--- src/api/ProductApi.ts
+++ src/api/ProductApi.ts
@@ -1,9 +1,11 @@
+import { Product } from "../types";
+
 const API_URL = "https://fakestoreapi.in/api/products?limit=15";
 
 export const fetchProducts = async (
   query: string,
   controller: AbortController
-) => {
+): Promise<Product[]> => {
   try {
     const response = await fetch(`${API_URL}&q=${query}`, {
       signal: controller.signal,
@@ -16,6 +18,7 @@ export const fetchProducts = async (
   } catch (err: any) {
     if (err.name === "AbortError") {
       console.log("Requested canceled", query);
+      return []; // fallback for canceled req
     } else {
       throw err;
     }
